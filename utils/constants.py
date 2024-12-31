fb00325c6d843cac1b4480ad79db46bed0f51022
Fred Bagi <fred1986zet@gmail.com>
2024-12-31 04:34:41 +0400

    news,constants,categories changed

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,16 @@
-AVAILABLE_LANGUAGES = {"ru": "Русский", "en": "English", "es": "Español", "de": "Deutsch"}
-CATEGORIES = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
-HELP = ["help"]
+AVAILABLE_LANGUAGES = {"ru": "Русский",
+                       "uk": "Українська",
+                       "en": "English",
+                       "de": "Deutsch",
+                       "es": "Español"}
+CATEGORIES = ["business",
+              "entertainment",
+              "general",
+              "health",
+              "science",
+              "sports",
+              "technology",
+              "jikan_anime",
+              "animechan"]
+
+# HELP = ["help"]
